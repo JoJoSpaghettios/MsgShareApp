49b5dc9f8b2da2d41d44576db5338fcf99e5ff67
JoJoSpaghettios <joe@jream.com>
2018-08-07 03:06:58 -0400

    Initial commit

---
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.smartherd.msgshareapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+/* Notes
+An activity represents a single screen with a user interface (layout).
+In this example, MainActivity.kt is an activity with UI (layout) of activity_main.xml.
+activity = kotlin file
+layout = xml file
+views = TextView, checkBox, Button, ListView, ImageView, etc ...
+ */
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was clicked!")
+
+            Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
