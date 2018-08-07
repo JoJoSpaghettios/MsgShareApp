39ac233a286a53dd4bc2513109a8cdbb10f9d7ed
JoJoSpaghettios <joe@jream.com>
2018-08-07 04:25:08 -0400

    Section Two, Video Three

--- app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.smartherd.msgshareapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -16,16 +17,25 @@ views = TextView, checkBox, Button, ListView, ImageView, etc ...
 
 
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnShowToast.setOnClickListener {
             Log.i("MainActivity", "Button was clicked!")
-
             Toast.makeText(this, "Button was clicked!", Toast.LENGTH_SHORT).show()
         }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
